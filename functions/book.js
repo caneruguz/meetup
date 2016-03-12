aea79ed3c31b5c7991986a45b5581245d42a7dbd
Caner Uguz <caneruguz@gmail.com>
2016-03-12 09:31:02 -0500

    Add closure example

--- functions/book.js
+++ functions/book.js
@@ -78,3 +78,23 @@ function checkscope() {
 	return f;
 }
 //checkscope()() // What does this return?
+
+
+// This function returns a function that always returns v
+function constfunc(v) { return function() { return v; }; }
+// Create an array of constant functions:
+var funcs = [];
+for(var i = 0; i < 10; i++) funcs[i] = constfunc(i);
+// The function at array element 5 returns the value 5.
+//funcs[5]() // => 5
+
+// Return an array of functions that return the values 0-9
+function constfuncs() {
+	var funcs = [];
+	for(var i = 0; i < 10; i++)
+		funcs[i] = function() { return i; };
+	return funcs;
+}
+var funcs = constfuncs();
+funcs[5]() // What does this return?
+
