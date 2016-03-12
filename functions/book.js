3dafe5b509559bdfd05b7841e3ed0dd423451403
Caner Uguz <caneruguz@gmail.com>
2016-03-12 07:49:20 -0500

    Add functions from book

--- functions/book.js
+++ functions/book.js
@@ -21,11 +21,42 @@ function factorial(x) {
 // Note that we assign it to a variable
 var square = function(x) { return x*x; };
 
-// Function expressions can include names, which is useful for recursion.
-var f = function fact(x) { if (x <= 1) return 1; else return x*fact(x-1); };
-
-// Function expressions can also be used as arguments to other functions:
-data.sort(function(a,b) { return a-b; });
-
 // Function expressions are sometimes defined and immediately invoked:
 var tensquared = (function(x) {return x*x;}(10));
+
+// Nested function
+function hypotenuse(a, b) {
+	function square(x) { return x*x; }
+	return Math.sqrt(square(a) + square(b));
+}
+
+// Function invocation
+printprops({x:1});
+var total = distance(0,0,2,1) + distance(2,1,3,5);
+var probability = factorial(5)/factorial(13);
+
+// Method
+var calculator = { // An object literal
+	operand1: 1,
+	operand2: 1,
+	add: function() {
+		// Note the use of the this keyword to refer to this object.
+		this.result = this.operand1 + this.operand2;
+	}
+};
+
+// this keyword
+var o = { // An object o.
+	m: function() { // Method m of the object.
+		var self = this; // Save the this value in a variable.
+		console.log(this === o); // Prints "true": this is the object o.
+
+		f(); // Now call the helper function f().
+
+		function f() { // A nested function f
+			console.log(this === o); // "false": this is global or undefined
+			console.log(self === o); // "true": self is the outer this value.
+		}
+	}
+};
+o.m();
